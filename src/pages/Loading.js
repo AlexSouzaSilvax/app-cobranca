4e150e7eeae314d7e96e068fe0cc7d282fb2a9a4
alexsouzasilvax <alexsouzasilvax@gmail.com>
2020-06-15 18:01:07 -0300

    novo splash, Loading

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,72 +1,91 @@
-import React, { useState, useEffect } from "react";
-import { Image, SafeAreaView, StyleSheet } from "react-native";
-import { ProgressBar } from "react-native-paper";
-import logo from "../../assets/icon.png";
-import { helper } from "../api";
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text, StyleSheet, Animated, StatusBar, BackHandler } from 'react-native';
+
+import { Actions } from 'react-native-router-flux';
+import Logo from "../../assets/icon.png";
+import { helper } from '../api';
+
+const switchToAuth = () => {
+  Actions.replace('auth')
+};
 
 export default function Loading({ navigation }) {
-  const [progresso, setProgresso] = useState(0);
+  const [logoAnime, setLogoAnime] = useState(new Animated.Value(0));
+  const [logoText, setLogoText] = useState(new Animated.Value(0));
 
-  function animacao() {
-    let progress = 0;
-    setTimeout(() => {
-      setInterval(() => {
-        progress += Math.random() / 5;
-        if (progress > 1) {
-          progress = 1;
-        }
-        setProgresso(progress);
-      }, 500);
-    }, 1500);
-  }
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(logoAnime, {
+        toValue: 1,
+        tension: 10,
+        friction: 2,
+        duration: 4000,//1000,
+      }).start(),
+      Animated.timing(logoText, {
+        toValue: 1,
+        duration: 1500,//1200,
+      }),
+    ]).start(() => {
+      //quando acabar animacao
+      verificaUsuarioLogado();
+      setTimeout(switchToAuth, 1500);
+    });
+  }, []);
 
   async function verificaUsuarioLogado() {
     await helper.getItem("idUsuario").then(idUsuario => {
       if (idUsuario) {
         navigation.navigate("Principal");
-        //console.log(`navigation.navigate("Principal");`);
       } else {
         navigation.navigate("Login");
-        //console.log(`navigation.navigate("Login");`);
       }
     });
   }
 
-  useEffect(() => {
-    animacao();
-    verificaUsuarioLogado();
-  }, []);
+  BackHandler.addEventListener("hardwareBackPress", () => {
+    navigation.navigate("Loading");
+    return true;
+  });
 
   return (
-    <SafeAreaView behavior="padding" style={styles.container}>
-      <Image source={logo} style={styles.logo} />
-      <ProgressBar
-        style={{ width: 200, marginTop: 50 }}
-        progress={progresso}
-        color={"#ddd"}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <Animated.View
+        style={{
+          opacity: logoAnime,
+          top: logoAnime.interpolate({
+            inputRange: [0, 1],
+            outputRange: [80, 0],
+          }),
+        }}>
+        <Image source={Logo} style={styles.logo} />
+      </Animated.View>
+      <Animated.View style={{ opacity: logoText }}>
+        <Text style={styles.logoText}> Cobrança </Text>
+      </Animated.View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#444",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#444',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  textLoading: {
-    paddingTop: 15,
-    fontSize: 20,
-    color: "#F3F3F3",
-    alignItems: "center",
-    justifyContent: "center"
+  logoText: {
+    color: '#F3F3F3',
+    fontSize: 58,
+    marginTop: 10,
+    fontWeight: '500',
+    fontFamily: "Chewy",
   },
   logo: {
+    marginTop: -150,
     width: 200,
     height: 200,
     alignSelf: "center",
-    resizeMode: "cover"
-  }
+    resizeMode: "cover",
+  },
 });
